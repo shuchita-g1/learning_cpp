7e8820d262ba896a4f866caa5b96410e02ccfa38
itsmemukund <32777662+itsmemukund@users.noreply.github.com>
2018-10-30 23:21:10 +0530

    indented code

--- Abc.cpp
+++ Abc.cpp
@@ -1,14 +1,11 @@
-class Abc
-{
+class Abc{
  int i;           //data variable
- void display()         //Member Function
-  { 
+ void display(){         //Member Function 
    cout<<"Inside Member Function";
   }
 }; // Class ends here
 
-int main()
-{
+int main(){
  Abc obj;  // Creatig Abc class's object
  obj.display();  //Calling member function using class object
 }
